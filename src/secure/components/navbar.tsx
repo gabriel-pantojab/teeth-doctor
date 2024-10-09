6a53d4185f26fc9521c936ca50e95489e03d2f21
Gabriel Bustamante <gabrieljuniorpb990@gmail.com>
2024-10-09 08:02:16 -0300

    feat: add navbar

---
+++ src/secure/components/navbar.tsx
@@ -0,0 +1,15 @@
+export function Navbar() {
+  return (
+    <nav className="p-2 border-t-2 border-gray-300 rounded-md">
+      <ul className="w-full flex justify-center gap-3">
+        <li>
+          <a href="#">Leccion</a>
+        </li>
+
+        <li>
+          <a href="#">Juegos</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
