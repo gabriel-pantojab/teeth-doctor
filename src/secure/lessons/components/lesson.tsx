edfcb5d0913e701c6b1d9b6ffb6ae729355aa54e
Gabriel Bustamante <gabrieljuniorpb990@gmail.com>
2024-10-13 18:40:19 -0300

    feat: add lessons

--- src/secure/lessons/components/lesson.tsx
+++ src/secure/lessons/components/lesson.tsx
@@ -1,17 +1,22 @@
 interface LessonProps {
   title: string;
-  video: string;
+  video?: string;
+  image?: string;
 }
 
-export function Lesson({ title, video }: LessonProps) {
+export function Lesson({ title, video, image }: LessonProps) {
   return (
     <section className="border-2 rounded-md p-2 flex flex-col gap-2 bg-white">
       <header className="flex justify-center">
-        <h2 className="font-bold">{title}</h2>
+        <h2 className="font-bold text-center">{title}</h2>
       </header>
 
-      <section className="flex border-2 rounded-md h-96 w-full overflow-hidden">
-        <video className="w-full object-cover" src={video} controls></video>
+      <section className="flex border-2 rounded-md max-h-96 w-full overflow-hidden">
+        {video ? (
+          <video className="w-full object-cover" src={video} controls></video>
+        ) : (
+          <img className="w-full object-contain" src={image} alt={title} />
+        )}
       </section>
     </section>
   );
--- src/secure/lessons/lesson-page.tsx
+++ src/secure/lessons/lesson-page.tsx
@@ -8,6 +8,26 @@ export function LessonPage() {
           title="Como lavarse los dientes"
           video="/assets/videos/lesson-1.mp4"
         />
+
+        <Lesson
+          title="1. Primero limpiaremos las caras vestibulares de los dientes."
+          image="https://res.cloudinary.com/dq6tbuvti/image/upload/e_gen_restore//v1728846755/step1_xvqu50.png"
+        />
+
+        <Lesson
+          title="2. Coge el cepillo y colócalo formando un ángulo de 45 grados respecto a la encía. Realiza movimientos circulares desde la encía al diente."
+          image="https://res.cloudinary.com/dq6tbuvti/image/upload/e_gen_restore/v1728847243/step2_rngkcz.png"
+        />
+
+        <Lesson
+          title="3. Continuamos con la cara interna de los dientes. Se coloca el cepillo de la misma forma, repitiendo el movimiento."
+          image="https://res.cloudinary.com/dq6tbuvti/image/upload/e_gen_restore/v1728857019/step3_osqvje.png"
+        />
+
+        <Lesson
+          title="4. Por último cepilla la cara de los dientes por la que masticamos (oclusal), realizando movimiento de barrido."
+          image="https://res.cloudinary.com/dq6tbuvti/image/upload/e_gen_restore/v1728857182/step4_ujna3q.png"
+        />
       </section>
     </section>
   );
